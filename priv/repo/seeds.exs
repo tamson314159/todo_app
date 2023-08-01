7c7c96c58912e3e5a85a2612c4e7ce359b6d363a
tamson314159 <tamson314159@gmail.com>
2023-08-01 11:06:14 +0900

    シードデータの追加 #1

--- priv/repo/seeds.exs
+++ priv/repo/seeds.exs
@@ -1,11 +1,36 @@
-# Script for populating the database. You can run it as:
-#
-#     mix run priv/repo/seeds.exs
-#
-# Inside the script, you can read and write to any of your
-# repositories directly:
-#
-#     TodoApp.Repo.insert!(%TodoApp.SomeSchema{})
-#
-# We recommend using the bang functions (`insert!`, `update!`
-# and so on) as they will fail if something goes wrong.
+alias TodoApp.Repo
+alias TodoApp.Accounts.Account
+alias TodoApp.Tasks.Task
+
+accounts = [
+  {"user01@sample.com", "user01999"},
+  {"user02@sample.com", "user02999"}
+]
+
+[a01, a02] =
+  Enum.map(accounts, fn {email, password} ->
+    Repo.insert!(
+      %Account{
+        email: email,
+        hashed_password: Pbkdf2.hash_pwd_salt(password),
+        confirmed_at: NaiveDateTime.truncate(NaiveDateTime.utc_now(), :second)
+      }
+    )
+  end)
+
+tasks = [
+  {"掃除", Date.utc_today(), true, a01},
+  {"洗濯", Date.utc_today(), false, a01},
+  {"炊事", Date.utc_today(), false, a02}
+]
+
+Enum.map(tasks, fn {title, date, completed, account} ->
+  Repo.insert!(
+    %Task{
+      title: title,
+      date: date,
+      completed: completed,
+      account_id: account.id
+    }
+  )
+end)
